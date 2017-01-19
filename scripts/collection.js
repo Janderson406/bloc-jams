117438810fa9d4d0962a1c7550b8a7445a7b2bd2
Jon Anderson <jondanderson@gmail.com>
2017-01-18 20:34:30 -0700

    Checkpoint 24: DOM Scripting Collection View

---
+++ scripts/collection.js
@@ -0,0 +1,29 @@
+ var collectionItemTemplate =
+     '<div class="collection-album-container column fourth">'
+   + '  <img src="assets/images/album_covers/01.png"/>'
+   + '  <div class="collection-album-info caption">'
+   + '    <p>'
+   + '      <a class="album-name" href="album.html"> The Colors </a>'
+   + '      <br/>'
+   + '      <a href="album.html"> Pablo Picasso </a>'
+   + '      <br/>'
+   + '      X songs'
+   + '      <br/>'
+   + '    </p>'
+   + '  </div>'
+   + '</div>'
+ 
+ //To make the template a string, it must be wrapped in quotation marks. 
+ //The template could exist on one line, but for readability we return each line as we normally would in an HTML document. 
+ //To keep the string together, we use + at the start of each line, where it's easy to see its location.
+ 
+ window.onload = function() {
+     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+            //select the first (and only, as we've designed it) element with an album-covers class name
+     collectionContainer.innerHTML = '';
+            //assign an empty string to collectionContainer's innerHTML property to clear its content.
+     for (var i = 0; i < 12; i++)  {
+         collectionContainer.innerHTML += collectionItemTemplate;
+            //inserts 12 albums     
+     }
+ }
