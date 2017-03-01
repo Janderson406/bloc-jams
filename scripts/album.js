4988da825f3a4066114f58a6f334ce8e747df375
Jon Anderson <jondanderson@gmail.com>
2017-02-28 21:35:55 -0700

    Assignment 32: Next Prev jQuery

--- scripts/album.js
+++ scripts/album.js
@@ -10,7 +10,17 @@
       + '</tr>'
       ;
  
-     var $row = $(template);
+    
+ var setSong = function(songNumber){
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+ }
+ 
+ var getSongNumberCell = function(number){
+    return $('.song-item-number[data-song-number="' + number + '"]');   
+ }
+    
+    var $row = $(template);
 
 
     var clickHandler = function() {
@@ -18,14 +28,13 @@
 
         if (currentlyPlayingSongNumber !== null) {
             // Revert to song number for currently playing song because user started playing new song.
-            var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+            var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
             currentlyPlayingCell.html(currentlyPlayingSongNumber);
         }
         if (currentlyPlayingSongNumber !== songNumber) {
             // Switch from Play -> Pause button to indicate new song is playing.
             $(this).html(pauseButtonTemplate);
-            currentlyPlayingSongNumber = songNumber;
-            currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+            setSong(songNumber);
              updatePlayerBarSong();            
         } else if (currentlyPlayingSongNumber === songNumber) {
             // Switch from Pause -> Play button to pause currently playing song.
